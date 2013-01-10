e24d064f51639e6bbe34502940c1b8b7b6d0364b
Michael Craft <mcraft@peak15.org>
2013-01-10 17:53:19 -0500

    And the wheel of progress slowly turn...

--- src/org/peak15/newlife/Token.java
+++ src/org/peak15/newlife/Token.java
@@ -1,8 +1,39 @@
 package org.peak15.newlife;
 
 /**
- * 
+ * One nicely packaged unit of source code!
  */
-public class Token {
+public final class Token {
 
+	public enum TokenType {
+		KEYWORD,
+		IDENTIFIER,
+		CONDITION,
+		WHITESPACE,
+		COMMENT,
+		ERROR,
+		EOF_TOKEN
+	}
+	
+	private final TokenType type;
+	private final String text;
+	
+	public Token(TokenType type, String text) {
+		this.type = type;
+		this.text = text;
+	}
+	
+	/**
+	 * @return the type of the token
+	 */
+	public TokenType getType() {
+		return type;
+	}
+
+	/**
+	 * @return the token's text
+	 */
+	public String getText() {
+		return text;
+	}
 }
--- src/org/peak15/newlife/Tokenizer.java
+++ src/org/peak15/newlife/Tokenizer.java
@@ -1,20 +1,34 @@
 package org.peak15.newlife;
 
+import java.io.InputStream;
+
 /**
  * Creates tagged tokens from an input stream.
  * 
  * @author Michael Craft <mcraft@peak15.org>
  */
 public class Tokenizer {
-	public enum TokenType {
-		KEYWORD,
-		IDENTIFIER,
-		CONDITION,
-		WHITESPACE,
-		COMMENT,
-		ERROR,
-		EOF_TOKEN
+	
+	private enum BufferState {
+		EMPTY_BS,
+		ID_KW_COND_BS,
+		WHITESPACE_BS,
+		COMMENT_BS,
+		ERROR_BS
 	}
 	
+	private BufferState bufferState = BufferState.EMPTY_BS;
+	private final StringBuilder buffer = new StringBuilder();
 	
+	private final InputStream ins;
+	
+	public Tokenizer(InputStream ins) {
+		this.ins = ins;
+	}
+	
+	public Token nextToken() {
+		return null;
+	}
+	
+	// hasNextToken?
 }
