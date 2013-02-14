5b3d4db15570e929fc2d760f7058988004a9b37e
Michael Craft <mcraft@peak15.org>
2013-02-14 01:01:01 -0500

    Created generic value type test template.

--- src/org/peak15/newlife/test/types/TokenTest.java
+++ src/org/peak15/newlife/test/types/TokenTest.java
@@ -6,25 +6,18 @@ import org.junit.Test;
 import org.peak15.newlife.types.Token;
 import org.peak15.newlife.types.Token.TokenType;
 
-public final class TokenTest {
+public final class TokenTest extends ValueTypeTest<Token> {
+	
+	@Override
+	protected Token A() {
+		return new Token(TokenType.COMMENT, "blarg");
+	}
 
-	private final Token a = new Token(TokenType.COMMENT, "blarg");
-	private final int aHash = a.hashCode();
-	private final Token b = new Token(TokenType.COMMENT, "blarg");
-	private final boolean ABEqual = a.equals(b);
-	private final Token d = new Token(TokenType.IDENTIFIER, "blarg");
-	private final Token c = new Token(TokenType.COMMENT, "blarg");
-
-	@Test
-	public void consistentHashCode() {
-		assertEquals(aHash, a.hashCode());
+	@Override
+	protected Token D() {
+		return new Token(TokenType.IDENTIFIER, "blarg");
 	}
 	
-	@Test
-	public void equalHashCode() {
-		assertEquals(a.hashCode(), b.hashCode());
-	}
-
 	@Test
 	public void testGetType() {
 		assertEquals(TokenType.COMMENT, a.getType());
@@ -33,40 +26,5 @@ public final class TokenTest {
 	@Test
 	public void testGetText() {
 		assertEquals(TokenType.COMMENT, a.getType());
-	}
-
-	@Test
-	public void reflexiveEquals() {
-		assertTrue("x.equals(x) == true", a.equals(a));
-	}
-	
-	@Test
-	public void symmetricEquals() {
-		// equal test
-		assertEquals("x.equals(y) iff y.equals(x)", a.equals(b), b.equals(a));
-		// unequal test
-		assertEquals("x.equals(y) iff y.equals(x)", a.equals(d), d.equals(a));
-	}
-	
-	@Test
-	public void transitiveEquals() {
-		assertEquals("if x.equals(y) && y.equals(z), then x.equals(z)",
-				a.equals(b) && b.equals(c),
-				a.equals(c));
-	}
-	
-	@Test
-	public void consistentEquals() {
-		assertEquals(ABEqual, a.equals(b));
-	}
-	
-	/**
-	 * This test always succeeds,
-	 * it exists to allow visual inspection of
-	 * the non explicitly defined toString() output.
-	 */
-	@Test
-	public void testToString() {
-		System.out.println(a.toString());
 	}
 }
---
+++ src/org/peak15/newlife/test/types/ValueTypeTest.java
@@ -0,0 +1,80 @@
+package org.peak15.newlife.test.types;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Template for tests on value types.
+ * Includes tests for equals, hashCode, and toString.
+ * 
+ * A and D should construct new objects s.t. A /= B
+ * 
+ * A and D should not be used by the implementing class.
+ * 
+ * a-c will be populated from A().
+ * d will be populated from D().
+ *
+ * @param <T> type to test
+ */
+public abstract class ValueTypeTest<T> {
+
+	
+	protected final T a = A();
+	protected final T b = A();
+	protected final T c = A();
+	protected final T d = D();
+	protected final int aHash = a.hashCode();
+	protected final boolean ABEqual = a.equals(b);
+
+	abstract protected T A();
+	abstract protected T D();
+
+	@Test
+	public void consistentHashCode() {
+		assertEquals(aHash, a.hashCode());
+	}
+
+	@Test
+	public void equalHashCode() {
+		assertEquals(a.hashCode(), b.hashCode());
+	}
+
+	@Test
+	public void reflexiveEquals() {
+		assertTrue("x.equals(x) == true", a.equals(a));
+		assertTrue("x.equals(x) == true", a.equals(b));
+	}
+
+	@Test
+	public void symmetricEquals() {
+		// equal test
+		assertEquals("x.equals(y) iff y.equals(x)", a.equals(b), a.equals(b));
+		// unequal test
+		assertEquals("x.equals(y) iff y.equals(x)", a.equals(d), d.equals(a));
+	}
+
+	@Test
+	public void transitiveEquals() {
+		assertEquals("if x.equals(y) && y.equals(z), then x.equals(z)",
+				a.equals(b) && b.equals(c),
+				a.equals(c));
+	}
+
+	@Test
+	public void consistentEquals() {
+		assertEquals(ABEqual, a.equals(b));
+	}
+
+	/**
+	 * This test always succeeds,
+	 * it exists to allow visual inspection of
+	 * the non explicitly defined toString() output.
+	 */
+	@Test
+	public void testToString() {
+		System.out.println(a.toString());
+	}
+
+}
