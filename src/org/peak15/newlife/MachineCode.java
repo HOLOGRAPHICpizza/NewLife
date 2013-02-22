1bfd9590dfcb2e762032904eb2a0522a50c660b1
Michael Craft <mcraft@peak15.org>
2013-02-22 09:27:42 -0500

    Decked out MachineCode

--- src/org/peak15/newlife/MachineCode.java
+++ src/org/peak15/newlife/MachineCode.java
@@ -1,21 +1,35 @@
 package org.peak15.newlife;
 
-public class MachineCode {
-	public static final byte MOVE = 0x00;
-	public static final byte TURNLEFT = 0x01;
-	public static final byte TURNRIGHT = 0x02;
-	public static final byte INFECT = 0x03;
-	public static final byte SKIP = 0x04;
-	public static final byte HALT = 0x05;
-	public static final byte JUMP = 0x06;
-	public static final byte JUMP_IF_NOT_NEXT_IS_EMPTY = 0x07;
-	public static final byte JUMP_IF_NOT_NEXT_IS_NOT_EMPTY = 0x08;
-	public static final byte JUMP_IF_NOT_NEXT_IS_WALL = 0x09;
-	public static final byte JUMP_IF_NOT_NEXT_IS_NOT_WALL = 0x0A;
-	public static final byte JUMP_IF_NOT_NEXT_IS_FRIEND = 0x0B;
-	public static final byte JUMP_IF_NOT_NEXT_IS_NOT_FRIEND = 0x0C;
-	public static final byte JUMP_IF_NOT_NEXT_IS_ENEMY = 0x0D;
-	public static final byte JUMP_IF_NOT_NEXT_IS_NOT_ENEMY = 0x0E;
-	public static final byte JUMP_IF_NOT_RANDOM = 0x0F;
-	public static final byte JUMP_IF_NOT_TRUE = 0x10;
+public enum MachineCode {
+	MOVE(0x00),
+	TURNLEFT(0x01),
+	TURNRIGHT(0x02),
+	INFECT(0x03),
+	SKIP(0x04),
+	HALT(0x05),
+	JUMP(0x06),
+	JUMP_IF_NOT_NEXT_IS_EMPTY(0x07),
+	JUMP_IF_NOT_NEXT_IS_NOT_EMPTY(0x08),
+	JUMP_IF_NOT_NEXT_IS_WALL(0x09),
+	JUMP_IF_NOT_NEXT_IS_NOT_WALL(0x0A),
+	JUMP_IF_NOT_NEXT_IS_FRIEND(0x0B),
+	JUMP_IF_NOT_NEXT_IS_NOT_FRIEND(0x0C),
+	JUMP_IF_NOT_NEXT_IS_ENEMY(0x0D),
+	JUMP_IF_NOT_NEXT_IS_NOT_ENEMY(0x0E),
+	JUMP_IF_NOT_RANDOM(0x0F),
+	JUMP_IF_NOT_TRUE(0x10);
+	
+	private final byte value;
+	
+	private MachineCode(int value) {
+		this((byte) value);
+	}
+	
+	private MachineCode(byte value) {
+		this.value = value;
+	}
+	
+	public byte value() {
+		return value;
+	}
 }
