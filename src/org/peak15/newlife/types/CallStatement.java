9f63bc48852183613a7b0b0952b267d6346ce188
Michael Craft <mcraft@peak15.org>
2013-02-21 23:23:14 -0500

    Implemented common Object methods on CallStatement.

--- src/org/peak15/newlife/types/CallStatement.java
+++ src/org/peak15/newlife/types/CallStatement.java
@@ -5,6 +5,13 @@ public final class CallStatement implements Statement {
 	private final String call;
 	
 	public CallStatement(String call) {
+		if(call == null) {
+			throw new NullPointerException("Call string may not be null.");
+		}
+		else if(call.length() < 1) {
+			throw new IllegalArgumentException("Call string may not be empty.");
+		}
+		
 		this.call = call;
 	}
 	
@@ -12,4 +19,30 @@ public final class CallStatement implements Statement {
 		return call;
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == this) {
+			return true;
+		}
+		
+		if(!(obj instanceof CallStatement)) {
+			return false;
+		}
+		
+		CallStatement s = (CallStatement) obj;
+		
+		return s.getCall().equals(this.getCall());
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = 9001;
+		result = 1327 * result + this.getCall().hashCode();
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("(CALL: %s)", this.getCall());
+	}
 }
